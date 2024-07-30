2d9bcdbdda2112bd96ccc11d279f43154b3574ac
Samuel Margerison <SamR.Margerison@gmail.com>
2024-07-30 12:56:31 +0100

    Working Simple 2D Boids simulation

--- boid.js
+++ boid.js
@@ -1,17 +1,127 @@
 class Boid {
     constructor() {
-        this.position = createVector();
-        this.velocity = createVector();
+        this.position = createVector(random(width), random(height));
+        this.velocity = p5.Vector.random2D();
         this.acceleration = createVector();
+        this.maxForce = 0.1;
+        this.maxSpeed = 1.5;
+    }  
+
+
+    edges() {
+        if (this.position.x > width) {
+            this.position.x = 0;
+        } else if (this.position.x < 0) {
+            this.position.x = width;
+        }
+        if (this.position.y > height) {
+            this.position.y = 0;
+        } else if (this.position.y < 0) {
+            this.position.y = height;
+        }
     }
 
+    align(boids) {
+        let perceptionRadius = 50;
+        let steering = createVector();
+        var total = 0;
+        for(let other of boids) {
+            let d = dist(
+                this.position.x, 
+                this.position.y, 
+                other.position.x, 
+                other.position.y
+            );
+
+            if (other != this && d<perceptionRadius) {
+                steering.add(other.velocity);
+                total++;
+            }
+
+        }
+        if (total > 0) {
+            steering.div(total);
+            steering.setMag(this.maxSpeed);
+            steering.sub(this.velocity);
+            steering.limit(this.maxForce);
+        }
+        return steering;
+    }
+
+    cohesion(boids) {
+        let perceptionRadius = 50;
+        let steering = createVector();
+        var total = 0;
+        for(let other of boids) {
+            let d = dist(
+                this.position.x, 
+                this.position.y, 
+                other.position.x, 
+                other.position.y
+            );
+
+            if (other != this && d<perceptionRadius) {
+                steering.add(other.position);
+                total++;
+            }
+
+        }
+        if (total > 0) {
+            steering.div(total);
+            steering.sub(this.position); // C
+            steering.setMag(this.maxSpeed);
+            steering.sub(this.velocity);
+            steering.limit(this.maxForce);
+        }
+        return steering;
+    }
+
+    separation(boids) {
+        let perceptionRadius = 50;
+        let steering = createVector();
+        var total = 0;
+        for(let other of boids) {
+            let d = dist(
+                this.position.x, 
+                this.position.y, 
+                other.position.x, 
+                other.position.y
+            );
+
+            if (other != this && d<perceptionRadius) {
+                let diff = p5.Vector.sub(this.position, other.position); // S
+                diff.div(d); //S
+                steering.add(diff);
+                total++;
+            }
+
+        }
+        if (total > 0) {
+            steering.div(total);
+            steering.setMag(this.maxSpeed);
+            steering.sub(this.velocity);
+            steering.limit(this.maxForce);
+        }
+        return steering;
+    }
+
+    flock(boids) {
+        this.acceleration.mult(0);
+        let alignment = this.align(boids);
+        let cohesion = this.cohesion(boids);
+        let separation = this.separation(boids);
+        this.acceleration.add(separation);
+        this.acceleration.add(alignment);
+        this.acceleration.add(cohesion);
+    }
+    
     update() {
         this.position.add(this.velocity);
-        this.position.add(this.acceleration);
+        this.velocity.add(this.acceleration);
     }
 
     show() {
-        strokeWeight(16);
+        strokeWeight(5);
         stroke(255);
         point(this.position.x, this.position.y);
         
--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 const flock = [];
 
 function setup() {
-    createCanvas(640, 360);
-    flock.push(new Boid())
+    createCanvas(640, 640);
+    for (let i = 1; i <= 200; i++) {
+        flock.push(new Boid());
+    }
 }
 
 function draw() {
     background(51);
-
     for (let boid of flock) {
+        boid.edges();
+        boid.flock(flock);
         boid.update();
-        boid.show()
+        boid.show();
     }
 }
